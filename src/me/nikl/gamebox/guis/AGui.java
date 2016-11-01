18fc7d447b4b769ac5d874a12d3b601bb6720dd3
BuildTools <unconfigured@null.spigotmc.org>
2016-11-01 11:46:39 +0400

    fields are protected + debug messages + removePlayer methods

--- src/me/nikl/gamebox/guis/AGui.java
+++ src/me/nikl/gamebox/guis/AGui.java
@@ -1,10 +1,12 @@
 package me.nikl.gamebox.guis;
 
+import me.nikl.gamebox.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,34 +18,66 @@ import java.util.UUID;
 public abstract class AGui implements IGui{
 	
 	// parent and child AGui
-	private AGui parentAGui;
-	private AGui childAGui;
+	protected IGui parentGui;
+	protected IGui childGui;
 	
-	private IButton[] grid;
+	protected IButton[] grid;
 	
 	
 	// inventory
-	private Inventory inv;
+	protected Inventory inv;
 	
 	// set of all players in the AGui
-	private Set<UUID> players;
+	protected Set<UUID> players;
 	
-	public AGui(AGui parentAGui, AGui childAGui, InventoryType invType){
-		this.parentAGui = parentAGui;
-		this.childAGui = childAGui;
+	public AGui(IGui parentAGui, IGui childAGui, InventoryType invType){
+		this.parentGui = parentAGui;
+		this.childGui = childAGui;
+		this.players = new HashSet<>();
+		
+		this.grid = new IButton[invType.getDefaultSize()];
 		
 		this.inv = Bukkit.createInventory(null, invType, "");
 	}
 	
-	@Override
-	public void openGui(UUID player){
-		Player pPlayer = Bukkit.getPlayer(player);
-		if(pPlayer != null) openGui(pPlayer);
+	public AGui(int numSlots){
+		this.parentGui = null;
+		this.childGui = null;
+		this.players = new HashSet<>();
+		
+		this.grid = new IButton[numSlots];
+		this.inv = Bukkit.createInventory(null, numSlots, "");
 	}
 	
 	@Override
-	public void openGui(Player player){
+	public boolean openGui(UUID player, IGui from){
+		Player pPlayer = Bukkit.getPlayer(player);
+		if(pPlayer != null) return openGui(pPlayer, from);
+		return false;
+	}
+	
+	@Override
+	public boolean openGui(Player player, IGui from){
+		if(from != null) from.removePlayer(player);
 		players.add(player.getUniqueId());
+		if(Main.debug) Bukkit.getConsoleSender().sendMessage("added to " + this.toString());
 		player.openInventory(inv);
+		return true;
+	}
+	
+	
+	@Override
+	public void removePlayer(UUID player){
+		players.remove(player);
+	}
+	
+	@Override
+	public void removePlayer(Player player){
+		players.remove(player.getUniqueId());
+	}
+	
+	@Override
+	public boolean inGUI(UUID uuid){
+		return players.contains(uuid);
 	}
 }
