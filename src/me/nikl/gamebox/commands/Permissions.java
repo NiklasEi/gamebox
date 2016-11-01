32894346ba98bea6b53bfc8b72b3349f97c3d42d
BuildTools <unconfigured@null.spigotmc.org>
2016-11-01 11:37:53 +0400

    added play permissions and method getGamePerm

--- src/me/nikl/gamebox/commands/Permissions.java
+++ src/me/nikl/gamebox/commands/Permissions.java
@@ -1,5 +1,8 @@
 package me.nikl.gamebox.commands;
 
+import me.nikl.gamebox.EnumGames;
+import org.bukkit.Bukkit;
+
 /**
  * Created by niklas on 10/27/16.
  *
@@ -7,12 +10,25 @@ package me.nikl.gamebox.commands;
  * just change the permission nodes here
  */
 public enum Permissions {
-	CMD_MAIN_USE("gamebox.use"), CMD_MAIN_RELOAD("gamebox.reload");
+	CMD_MAIN_USE("gamebox.use"), CMD_MAIN_RELOAD("gamebox.reload"),
+	GAME_BATTLESHIP_PLAY("gamebox.battleship.play"), GAME_MINESWEEPER_PLAY("gamebox.minesweeper.play"), GAME_GEMCRUSH_PLAY("gamebox.gemcrush.play"),
+	GAME_TETRIS_PLAY("gamebox.tetris.play");
 	
 	public String perm;
 	
 	Permissions(String perm){
 		this.perm = perm;
+	}
+	
+	public static Permissions getGamePerm(EnumGames eGame, String specifyPerm){
+		String gameName = eGame.toString();
+		for(Permissions perm : Permissions.values()){
+			String[] name = perm.name().split("_");
+			if(!name[0].equalsIgnoreCase("game")) continue;
+			if(!(name.length == 3)) continue;
+			if(name[1].equalsIgnoreCase(gameName) && name[2].equalsIgnoreCase(specifyPerm)) return perm;
+		}
+		return null;
 	}
 	
 	String getPerm(){
