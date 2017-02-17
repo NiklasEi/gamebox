90c151e28d9f557fb8fbad75c19376e93920b3f2
Niklas Eicker <nikle1@gmx.de>
2017-02-17 04:01:54 +0100

    Timer class to reset the inventory title after a title message

---
+++ src/me/nikl/gamebox/guis/timer/TitleTimer.java
@@ -0,0 +1,39 @@
+package me.nikl.gamebox.guis.timer;
+
+import me.nikl.gamebox.GameBox;
+import me.nikl.gamebox.PluginManager;
+import me.nikl.gamebox.nms.NMSUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Created by Niklas on 16.02.2017.
+ */
+public class TitleTimer extends BukkitRunnable {
+    private String title;
+    private long timestamp;
+    private Player player;
+    private NMSUtil nms;
+    private PluginManager pluginManager;
+
+    public TitleTimer(GameBox plugin, String title, Player player, long timestamp){
+        this.title = title;
+        this.timestamp = timestamp;
+        this.player = player;
+        this.nms = plugin.getNMS();
+        this.pluginManager = plugin.getPluginManager();
+
+        runTaskTimer(plugin, 10,10);
+    }
+
+
+    @Override
+    public void run() {
+        long currentTime = System.currentTimeMillis();
+        if(currentTime > timestamp){
+            nms.updateInventoryTitle(player, title);
+            pluginManager.removeTitleTimer(player.getUniqueId());
+        }
+    }
+}
