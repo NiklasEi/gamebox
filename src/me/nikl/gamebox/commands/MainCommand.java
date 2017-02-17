9d7a58765fc51e8f791958de570f042b726d7b8d
Niklas Eicker <nikle1@gmx.de>
2017-02-17 12:24:46 +0100

    added subcommands, help, info and the wrong usage message

--- src/me/nikl/gamebox/commands/MainCommand.java
+++ src/me/nikl/gamebox/commands/MainCommand.java
@@ -1,10 +1,11 @@
 package me.nikl.gamebox.commands;
 
+import me.nikl.gamebox.GameBox;
 import me.nikl.gamebox.Language;
-import me.nikl.gamebox.Main;
 import me.nikl.gamebox.Permissions;
 import me.nikl.gamebox.PluginManager;
 import me.nikl.gamebox.guis.GUIManager;
+import me.nikl.gamebox.guis.gui.game.GameGui;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,19 +13,26 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by niklas on 10/17/16.
  *
  * Command class for the essential plugin commands
  */
 public class MainCommand implements CommandExecutor{
-	private Main plugin;
+	private GameBox plugin;
 	private FileConfiguration config;
 	private PluginManager pManager;
 	private GUIManager guiManager;
 	private Language lang;
+
+	private Map<String, ArrayList<String>> subCommands = new HashMap<>();
 	
-	public MainCommand(Main plugin){
+	public MainCommand(GameBox plugin){
 		this.plugin = plugin;
 		this.pManager = plugin.getPluginManager();
 		this.guiManager = pManager.getGuiManager();
@@ -50,8 +58,56 @@ public class MainCommand implements CommandExecutor{
 			Player player = (Player) sender;
 			guiManager.openMainGui(player);
 			return true;
+		} else if(args.length == 1){
+			// check weather the given argument is a listed sub command
+			for(String id : subCommands.keySet()){
+				if(subCommands.get(id).contains(args[0].toLowerCase())){
+					if (!(sender instanceof Player)) {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', lang.PREFIX + lang.CMD_ONLY_PLAYER));
+						return true;
+					}
+					Player player = (Player) sender;
+					String[] arguments = new String[2];
+					arguments[0] = id; arguments[1] = GUIManager.MAIN_GAME_GUI;
+					guiManager.openGameGui(player, arguments);
+					return true;
+				}
+			}
+			// help command
+			if(args[0].equalsIgnoreCase("help") || args[0].equals("?")){
+				if(!sender.hasPermission(Permissions.CMD_HELP.getPermission())){
+					sender.sendMessage(lang.PREFIX + lang.CMD_NO_PERM);
+					return true;
+				}
+				for(String message : lang.CMD_HELP){
+					sender.sendMessage(lang.PREFIX + message);
+				}
+				return true;
+			}
+			// info command
+			if(args[0].equalsIgnoreCase("info")){
+				if(!sender.hasPermission(Permissions.CMD_INFO.getPermission())){
+					sender.sendMessage(lang.PREFIX + lang.CMD_NO_PERM);
+					return true;
+				}
+				sender.sendMessage(lang.PREFIX + " version " + plugin.getDescription().getVersion() + " by " + ChatColor.GOLD + "Nikl");
+				return true;
+			}
+			// not a listed sub command
+			// TodO: reload and other plugin commands
 		}
-		// ToDo: help message
+		// wrong usage message
+		for(String message : lang.CMD_WRONG_USAGE){
+			sender.sendMessage(lang.PREFIX + message);
+		}
 		return true;
+	}
+
+	public void registerSubCommands(String gameID, String... subCommands){
+		if(subCommands == null || subCommands.length < 1) return;
+		for(int i = 0; i < subCommands.length;i++){
+			subCommands[i] = subCommands[i].toLowerCase();
+		}
+		this.subCommands.put(gameID, new ArrayList<>(Arrays.asList(subCommands)));
 	}
 }
