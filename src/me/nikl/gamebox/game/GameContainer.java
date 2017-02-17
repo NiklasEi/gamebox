7cfca9e6e254a60fef63fb013d1b9a8d0701ce43
Niklas Eicker <nikle1@gmx.de>
2017-02-17 12:33:58 +0100

    container for game id, name and settings

---
+++ src/me/nikl/gamebox/game/GameContainer.java
@@ -0,0 +1,54 @@
+package me.nikl.gamebox.game;
+
+import me.nikl.gamebox.game.IGameManager;
+
+
+/**
+ * Created by Niklas on 14.02.2017.
+ *
+ *
+ */
+public class GameContainer {
+    private IGameManager gameManager;
+    private boolean handleClicksOnHotbar;
+    private String gameID;
+    private String plainName, name;
+
+
+    public GameContainer(String gameID, IGameManager gameManager){
+        this.gameManager = gameManager;
+        this.gameID = gameID;
+    }
+
+    public String getPlainName() {
+        return plainName;
+    }
+
+    public void setPlainName(String plainName) {
+        this.plainName = plainName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public boolean handleClicksOnHotbar() {
+        return handleClicksOnHotbar;
+    }
+
+    public void setHandleClicksOnHotbar(boolean handleClicksOnHotbar) {
+        this.handleClicksOnHotbar = handleClicksOnHotbar;
+    }
+
+    public IGameManager getGameManager() {
+        return gameManager;
+    }
+}
