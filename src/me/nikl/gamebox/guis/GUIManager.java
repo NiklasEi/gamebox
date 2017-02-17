16abba9d63d8b31b14d51f4f820b348f49e3c20f
Niklas Eicker <nikle1@gmx.de>
2017-02-17 15:53:20 +0100

    Title messages

--- src/me/nikl/gamebox/guis/GUIManager.java
+++ src/me/nikl/gamebox/guis/GUIManager.java
@@ -1,22 +1,23 @@
 package me.nikl.gamebox.guis;
 
 import me.nikl.gamebox.ClickAction;
+import me.nikl.gamebox.GameBox;
 import me.nikl.gamebox.Language;
-import me.nikl.gamebox.Main;
 import me.nikl.gamebox.Permissions;
+import me.nikl.gamebox.game.IGameManager;
 import me.nikl.gamebox.guis.button.AButton;
 import me.nikl.gamebox.guis.gui.AGui;
 import me.nikl.gamebox.guis.gui.MainGui;
+import me.nikl.gamebox.guis.gui.game.GameGui;
+import me.nikl.gamebox.guis.gui.game.GameGuiPage;
 import me.nikl.gamebox.nms.NMSUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.MaterialData;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,17 +32,19 @@ import java.util.logging.Level;
  * (not mainGUI and not game)
  */
 public class GUIManager implements Listener {
-	private Main plugin;
-	private Map<String, Map<String, AGui>> gameGuis;
+	private GameBox plugin;
+	private Map<String, Map<String, GameGui>> gameGuis;
 	private NMSUtil nms;
 	private Language lang;
 	
 	private MainGui mainGui;
 
-	public static final String MAIN_GAME_GUI = "main";
-	
-	
-	public GUIManager(Main plugin){
+	private int titleMessageSeconds = 3;
+
+	public static final String MAIN_GAME_GUI = "main", PLAY_GAME_GUI = "play";
+
+
+	public GUIManager(GameBox plugin){
 		this.plugin = plugin;
 		this.nms = plugin.getNMS();
 		this.lang = plugin.lang;
@@ -58,42 +61,47 @@ public class GUIManager implements Listener {
 		if (mainGui.isInGui(uuid)) {
 			event.setCancelled(true);
 			if(topInv)mainGui.onInvClick(event);
-			if(!topInv)Main.debug("lower inventory was clicked   TODO!!!");
+			if(!topInv)mainGui.onBottomInvClick(event);
 			return;
 		}
 		for (String gameID : gameGuis.keySet()) {
-			Map<String, AGui> guis = gameGuis.get(gameID);
-			for (AGui gui : guis.values()) {
+			Map<String, GameGui> guis = gameGuis.get(gameID);
+			for (GameGui gui : guis.values()) {
 				if (gui.isInGui(uuid)) {
 					event.setCancelled(true);
 					if(topInv)gui.onInvClick(event);
-					if(!topInv)Main.debug("lower inventory was clicked   TODO!!!");
+					if(!topInv)gui.onBottomInvClick(event);
 					return;
 				}
 			}
 		}
-		if(!topInv)Main.debug("lower inventory was clicked   TODO!!!");
-
-		if(Main.debug)Bukkit.getConsoleSender().sendMessage("Not in a GameBox GUI");
+		if(GameBox.debug)Bukkit.getConsoleSender().sendMessage("Not in a GameBox GUI");
 	}
-	
+
+
+
 	public void onInvClose(InventoryCloseEvent event) {
 		// get the uuid and check where the event should go
+		if(!(event.getPlayer() instanceof Player)) return;
 		UUID uuid = event.getPlayer().getUniqueId();
 		if(mainGui.isInGui(uuid)){
 			mainGui.onInvClose(event);
+			plugin.getPluginManager().restoreInventory((Player)event.getPlayer());
+			((Player) event.getPlayer()).updateInventory();
 			return;
 		}
 		for(String gameID : gameGuis.keySet()){
-			Map<String, AGui> guis = gameGuis.get(gameID);
-			for(AGui gui : guis.values()){
+			Map<String, GameGui> guis = gameGuis.get(gameID);
+			for(GameGui gui : guis.values()){
 				if(gui.isInGui(uuid)){
 					gui.onInvClose(event);
+					plugin.getPluginManager().restoreInventory((Player)event.getPlayer());
+					((Player) event.getPlayer()).updateInventory();
 					return;
 				}
 			}
 		}
-		if(Main.debug)Bukkit.getConsoleSender().sendMessage("Not in a GameBox GUI");
+		if(GameBox.debug)Bukkit.getConsoleSender().sendMessage("Not in a GameBox GUI");
 	}
 	
 	public boolean isInGUI(UUID uuid){
@@ -117,8 +125,8 @@ public class GUIManager implements Listener {
 	}
 
 	public boolean isInGameGUI(UUID uuid, String gameID){
-		Map<String, AGui> guis = gameGuis.get(gameID);
-		for(AGui gui : guis.values()){
+		Map<String, GameGui> guis = gameGuis.get(gameID);
+		for(GameGui gui : guis.values()){
 			if(gui.isInGui(uuid)){
 				return true;
 			}
@@ -127,50 +135,99 @@ public class GUIManager implements Listener {
 	}
 	
 	public boolean openGameGui(Player whoClicked, String... args) {
+		if(!plugin.getPluginManager().hasSavedContents(whoClicked.getUniqueId())){
+			plugin.getPluginManager().saveInventory(whoClicked);
+		}
 		if(args.length == 2) {
 			String gameID = args[0], key = args[1];
-			if (whoClicked.hasPermission(Permissions.OPEN_ALL_GAME_GUI.getPermission()) || whoClicked.hasPermission(Permissions.OPEN_GAME_GUI.getPermission(gameID))) {
-				return gameGuis.get(gameID).get(key).open(whoClicked);
+			if (whoClicked.hasPermission(Permissions.ADMIN.getPermission()) || whoClicked.hasPermission(Permissions.OPEN_ALL_GAME_GUI.getPermission())|| whoClicked.hasPermission(Permissions.OPEN_GAME_GUI.getPermission(gameID))) {
+				GameBox.openingNewGUI = true;
+				boolean opened = gameGuis.get(gameID).get(key).open(whoClicked);
+				GameBox.openingNewGUI = false;
+				if(opened){
+					nms.updateInventoryTitle(whoClicked, lang.TITLE_GAME_GUI.replace("%game%", plugin.getPluginManager().getGame(gameID).getName()).replace("%player%", whoClicked.getName()));
+				} else {
+					plugin.getPluginManager().restoreInventory(whoClicked);
+				}
+				return opened;
 			} else {
-				whoClicked.sendMessage(color(lang.CMD_NO_PERM));
+				if(mainGui.isInGui(whoClicked.getUniqueId())){
+
+					String currentTitle = plugin.lang.TITLE_MAIN_GUI.replace("%player%", whoClicked.getName());
+					plugin.getPluginManager().startTitleTimer(whoClicked, currentTitle, titleMessageSeconds);
+					plugin.getNMS().updateInventoryTitle(whoClicked, plugin.lang.TITLE_NO_PERM);
+				}
+				whoClicked.sendMessage(lang.PREFIX + lang.CMD_NO_PERM);
+				plugin.getPluginManager().restoreInventory(whoClicked);
 				return false;
 			}
 		} else {
 			Bukkit.getConsoleSender().sendMessage("unknown number of arguments in GUIManager.openGameGui");
+			plugin.getPluginManager().restoreInventory(whoClicked);
 			return false;
 		}
 	}
 	
 	public boolean openMainGui(Player whoClicked, String... args) {
+		if(!plugin.getPluginManager().hasSavedContents(whoClicked.getUniqueId())){
+			plugin.getPluginManager().saveInventory(whoClicked);
+		}
 		if(args == null || args.length==0){
 			if(whoClicked.hasPermission(Permissions.CMD_MAIN.getPermission())){
+				GameBox.openingNewGUI = true;
 				mainGui.open(whoClicked);
-				nms.updateInventoryTitle(whoClicked, lang.TITLE_MAIN_GUI);
+				GameBox.openingNewGUI = false;
 				return true;
 			}
-			//ToDo send message to player?
+			plugin.getPluginManager().restoreInventory(whoClicked);
+			whoClicked.sendMessage(lang.PREFIX + lang.CMD_NO_PERM);
 			return false;
 		}
 		Bukkit.getLogger().log(Level.WARNING, "in openMainGui not supported arg found: " + args.toString());
 		return false;
 	}
 
-	public void registerGameGUI(String gameID, String arg, AGui gui){
+	public void registerGameGUI(String gameID, String arg, GameGui gui){
 		gameGuis.computeIfAbsent(gameID, k -> new HashMap<>());
 
 		gameGuis.get(gameID).put(arg, gui);
-		Main.debug("registered gamegui: " + gameID + ", " + arg);
+		GameBox.debug("registered gamegui: " + gameID + ", " + arg);
 	}
 
-	public void registerGameGUI(String gameID, String arg, AGui gui, ItemStack button){
+	public void registerGameGUI(String gameID, String arg, GameGui gui, ItemStack button){
 		registerGameGUI( gameID,  arg,  gui);
 		AButton gameButton = new AButton(button.getData(), 1);
+		gameButton.setItemMeta(button.getItemMeta());
 		gameButton.setAction(ClickAction.OPEN_GAME_GUI);
 		gameButton.setArgs(gameID);
 		mainGui.setButton(gameButton);
 	}
 
+	public void registerGameGUI(String gameID, String arg, GameGui gui, ItemStack button, String... subCommand){
+		registerGameGUI(gameID, arg, gui, button);
+		if(subCommand != null)plugin.getMainCommand().registerSubCommands(gameID, subCommand);
+	}
+
 	private String color(String message){
 		return ChatColor.translateAlternateColorCodes('&', message);
+	}
+
+	public AGui getCurrentGui(UUID uuid){
+		if(mainGui.isInGui(uuid)){
+			return mainGui;
+		}
+		for(String gameID : gameGuis.keySet()){
+			if(isInGameGUI(uuid,gameID)){
+				for(GameGui gui : gameGuis.get(gameID).values()){
+					if(gui.isInGui(uuid)){
+						return gui;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public void shutDown() {
 	}
 }
