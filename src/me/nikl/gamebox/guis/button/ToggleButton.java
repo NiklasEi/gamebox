fbb39cefc40d91c8d0fdae9252afd2bd012cc275
Niklas Eicker <nikle1@gmx.de>
2017-02-17 12:35:01 +0100

    Button to toggle. Two sets of names/lore/material

---
+++ src/me/nikl/gamebox/guis/button/ToggleButton.java
@@ -0,0 +1,56 @@
+package me.nikl.gamebox.guis.button;
+
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Niklas on 17.02.2017.
+ *
+ */
+public class ToggleButton extends AButton {
+    private boolean toggled = false;
+    private MaterialData toggleData;
+    private String toggleDisplayName = "missing name";
+    private List<String> toggleLore = new ArrayList<>(Arrays.asList("missing lore"));
+
+
+    public ToggleButton(MaterialData mat, int count, MaterialData mat2) {
+        super(mat, count);
+        this.toggleData = mat2;
+    }
+
+    @SuppressWarnings("deprecation")
+    public ToggleButton toggle(){
+        toggled = !toggled;
+        MaterialData mat = toggleData;
+        ItemMeta meta = getItemMeta();
+        String displayName = toggleDisplayName;
+        ArrayList<String> lore = new ArrayList<>(toggleLore);
+
+        toggleData = getData();
+        toggleDisplayName = meta.getDisplayName();
+        toggleLore = new ArrayList<>(meta.getLore());
+
+        setData(mat);
+        setType(mat.getItemType());
+        setDurability(mat.getData());
+        meta.setDisplayName(displayName);
+        meta.setLore(lore);
+        meta.addItemFlags(ItemFlag.values());
+        setItemMeta(meta);
+        return this;
+    }
+
+    public void setToggleDisplayName(String toggleDisplayName){
+        this.toggleDisplayName = toggleDisplayName;
+    }
+
+    public void setToggleLore(List<String> toggleLore) {
+        this.toggleLore = toggleLore;
+    }
+}
