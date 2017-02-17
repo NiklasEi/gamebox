73d460a844a003c077acf1757e79773f25936272
Niklas Eicker <nikle1@gmx.de>
2017-02-17 12:22:26 +0100

    added action CLOSE and TOGGLE

--- src/me/nikl/gamebox/ClickAction.java
+++ src/me/nikl/gamebox/ClickAction.java
@@ -4,5 +4,5 @@ package me.nikl.gamebox;
  * Created by niklas on 2/5/17.
  */
 public enum ClickAction {
-	START_GAME, OPEN_GAME_GUI, OPEN_MAIN_GUI, NOTHING, CHANGE_GAME_GUI;
+	START_GAME, OPEN_GAME_GUI, OPEN_MAIN_GUI, NOTHING, CHANGE_GAME_GUI, CLOSE, TOGGLE;
 }
