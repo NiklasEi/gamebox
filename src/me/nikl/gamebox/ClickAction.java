7080410fb1f12b34526e9145be2fcedd8d434d1c
Niklas Eicker <nikle1@gmx.de>
2017-02-21 00:34:59 +0100

    added SHOW_TOP_LIST

--- src/me/nikl/gamebox/ClickAction.java
+++ src/me/nikl/gamebox/ClickAction.java
@@ -2,7 +2,9 @@ package me.nikl.gamebox;
 
 /**
  * Created by niklas on 2/5/17.
+ *
+ *
  */
 public enum ClickAction {
-	START_GAME, OPEN_GAME_GUI, OPEN_MAIN_GUI, NOTHING, CHANGE_GAME_GUI, CLOSE, TOGGLE;
+	START_GAME, OPEN_GAME_GUI, OPEN_MAIN_GUI, NOTHING, CHANGE_GAME_GUI, CLOSE, TOGGLE, SHOW_TOP_LIST
 }
