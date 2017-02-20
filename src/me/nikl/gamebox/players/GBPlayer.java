fb9db90ea72f09d9990521484c22901df787cbe0
Niklas Eicker <nikle1@gmx.de>
2017-02-21 00:46:43 +0100

    added remove and save method to handle shutdowns and data saving

--- src/me/nikl/gamebox/players/GBPlayer.java
+++ src/me/nikl/gamebox/players/GBPlayer.java
@@ -1,5 +1,8 @@
 package me.nikl.gamebox.players;
 
+import me.nikl.gamebox.GameBox;
+import me.nikl.gamebox.data.Statistics;
+
 import java.util.UUID;
 
 /**
@@ -9,9 +12,23 @@ import java.util.UUID;
 public class GBPlayer {
     private UUID uuid;
     private boolean playSounds = true;
+    private GameBox plugin;
+    private Statistics statistics;
 
-    public GBPlayer(UUID uuid){
+    public GBPlayer(GameBox plugin, UUID uuid){
         this.uuid = uuid;
+        this.plugin = plugin;
+        this.statistics = plugin.getStatistics();
+
+        loadData();
+        //plugin.getPluginManager().getGuiManager().getMainGui().loadMainGui(this);
+    }
+
+    private void loadData() {
+        // ToDo
+        if(statistics.isSet(uuid.toString())){
+            playSounds = statistics.getBoolean(uuid, Statistics.PLAYER_PLAY_SOUNDS, true);
+        }
     }
 
     public UUID getUuid() {
@@ -28,5 +45,18 @@ public class GBPlayer {
 
     public void toggleSound(){
         this.playSounds = !playSounds;
+    }
+
+    public void remove() {
+        //TOdO
+        // remove special inventories and save any data
+        // after this call this object will be removed
+        plugin.getPluginManager().getGuiManager().removePlayer(this.uuid);
+        save();
+    }
+
+    public void save() {
+        //go through all values and save them
+        statistics.set(uuid.toString(), Statistics.PLAYER_PLAY_SOUNDS, playSounds);
     }
 }
