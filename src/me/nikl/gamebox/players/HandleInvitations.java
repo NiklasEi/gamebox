b1674d0ec35459748c3facb94158dd81e0ef614c
Niklas Eicker <nikle1@gmx.de>
2017-02-23 01:37:07 +0100

    pass on invites to the corresponding gui. Made messages customizable

--- src/me/nikl/gamebox/players/HandleInvitations.java
+++ src/me/nikl/gamebox/players/HandleInvitations.java
@@ -2,6 +2,9 @@ package me.nikl.gamebox.players;
 
 import me.nikl.gamebox.GameBox;
 import me.nikl.gamebox.PluginManager;
+import me.nikl.gamebox.guis.gui.game.StartMultiplayerGamePage;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
@@ -14,9 +17,11 @@ import java.util.*;
 public class HandleInvitations extends BukkitRunnable{
     private Set<Invitation> invitations = new HashSet<>();
     private PluginManager pluginManager;
+    private GameBox plugin;
 
     public HandleInvitations(GameBox plugin){
         pluginManager = plugin.getPluginManager();
+        this.plugin = plugin;
         this.runTaskTimerAsynchronously(plugin, 20, 10);
     }
 
@@ -30,6 +35,7 @@ public class HandleInvitations extends BukkitRunnable{
             if(currentTimeMillis > inv.timeStamp){
                 ranOut.add(inv.player1);
                 it.remove();
+                ((StartMultiplayerGamePage)pluginManager.getGuiManager().getGameGui(inv.args[0], inv.args[1])).removeInvite(inv.player1, inv.player2);
                 GameBox.debug("removing old invitation");
             }
         }
@@ -39,17 +45,17 @@ public class HandleInvitations extends BukkitRunnable{
     public boolean addInvite(UUID player1, UUID player2, long timeStamp, String... args){
         for(Invitation inv : invitations){
             if(inv.player1.equals(player1) && inv.player2.equals(player2)){
-                GameBox.debug("There is already such an invitation");
-                //ToDo send message
+                Player player = Bukkit.getPlayer(player1);
+                if(player != null) player.sendMessage(plugin.lang.INVITATION_ALREADY_THERE);
                 return false;
             }
             if(inv.player1.equals(player2) && inv.player2.equals(player1)){
-                GameBox.debug("There is already an opposite inv. ToDo: accept");
                 //ToDO: accept invite automatically
-                return false;
+                // for now just continue and allow it
             }
         }
         new Invitation(player1, player2, timeStamp, args);
+        ((StartMultiplayerGamePage)pluginManager.getGuiManager().getGameGui(args[0], args[1])).addInvite(player1, player2);
         return true;
     }
 
