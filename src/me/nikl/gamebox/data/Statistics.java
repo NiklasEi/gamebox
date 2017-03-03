20d765b80724d00a31f3d4f7a9e4d54e9b9e3847
Niklas Eicker <nikle1@gmx.de>
2017-03-03 13:08:19 +0100

    abstract class now to support MySql

--- src/me/nikl/gamebox/data/Statistics.java
+++ src/me/nikl/gamebox/data/Statistics.java
@@ -12,163 +12,39 @@ import java.util.logging.Level;
 /**
  * Created by Niklas on 19.02.2017.
  */
-public class Statistics {
+public abstract class Statistics {
 
 
     public static final String PLAYER_PLAY_SOUNDS = "playSounds";
     public static final String GAMES_STATISTICS_NODE = "gameStatistics";
-    private File dataFile;
-    private FileConfiguration data;
     private GameBox plugin;
 
 
     public Statistics(GameBox plugin){
         this.plugin = plugin;
-
-
-        this.dataFile = new File(plugin.getDataFolder().toString() + File.separatorChar + "data.yml");
     }
 
 
-    public boolean load(){
-        if(!dataFile.exists()){
-            try {
-                dataFile.getParentFile().mkdir();
-                dataFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }
+    public abstract boolean load();
 
-        // load stats file
-        try {
-            this.data = YamlConfiguration.loadConfiguration(new InputStreamReader(new FileInputStream(this.dataFile), "UTF-8"));
-        } catch (UnsupportedEncodingException | FileNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+    public abstract boolean getBoolean(UUID uuid, String path);
 
 
-    public boolean isSet(String path) {
-        return data.isSet(path);
-    }
+    public abstract boolean getBoolean(UUID uuid, String path, boolean defaultValue);
 
-    public boolean getBoolean(UUID uuid, String path){
-        return getBoolean(uuid, path, false);
-    }
+    public abstract void set(String uuid, String path, Object b);
 
+    public abstract void save();
 
-    public boolean getBoolean(UUID uuid, String path, boolean defaultValue) {
-        return data.getBoolean(uuid + "." + path, defaultValue);
-    }
+    public abstract void addStatistics(UUID uuid, String gameID, String gameTypeID, double value, SaveType saveType);
 
-    public void set(String uuid, String path, Object b) {
-        data.set(uuid + "." + path, b);
-    }
-
-    public void save() {
-        try {
-            this.data.save(dataFile);
-        } catch (IOException e) {
-            Bukkit.getLogger().log(Level.SEVERE, "failed to save statistics", e);
-            e.printStackTrace();
-        }
-    }
-
-    public void addStatistics(UUID uuid, String gameID, String gameTypeID, double value, SaveType saveType){
-        GameBox.debug("Adding statistics '" +uuid.toString()+"."+gameID+"."+gameTypeID+ "." + saveType.toString().toLowerCase()+"' with the value: " + value);
-        double oldScore;
-        switch (saveType){
-            case SCORE:
-            case TIME_HIGH:
-                oldScore = data.getDouble(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), 0.);
-                if(oldScore >= value) return;
-                data.set(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), value);
-                break;
-
-            case TIME_LOW:
-                oldScore = data.getDouble(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), Double.MAX_VALUE);
-                if(oldScore <= value) return;
-                data.set(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), value);
-                break;
-
-            case WINS:
-                oldScore = data.getDouble(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), 0.);
-                data.set(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase(), value + oldScore);
-                break;
-
-            default:
-                Bukkit.getLogger().log(Level.WARNING, "trying to save unsupported statistics: " + saveType.toString());
-        }
-    }
-
-    public ArrayList<Stat> getTopList(String gameID, String gameTypeID, SaveType saveType, int maxNumber){
-        ArrayList<Stat> toReturn = new ArrayList<>();
-        Map<UUID, Double> valuesMap = new HashMap<>();
-        for(String uuid : data.getKeys(false)){
-            if(!data.isSet(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase())) continue;
-            try{
-                UUID uuid1 = UUID.fromString(uuid);
-                valuesMap.put(uuid1, data.getDouble(uuid.toString() + "."+ GAMES_STATISTICS_NODE+"." + gameID +"."+gameTypeID + "." + saveType.toString().toLowerCase()));
-            } catch (IllegalArgumentException exception){
-                exception.printStackTrace();
-                Bukkit.getLogger().log(Level.WARNING, "failed to load a statistic for a player due to a malformed UUID");
-                continue;
-            }
-        }
-
-        boolean higher;
-        switch (saveType){
-            case TIME_LOW:
-                higher = false;
-                break;
-            case WINS:
-            case TIME_HIGH:
-            case SCORE:
-                higher = true;
-                break;
-
-            default:
-                higher = true;
-                Bukkit.getLogger().log(Level.SEVERE, "not supported SaveType found while loading a top list");
-        }
-        UUID currentBestUuid = null;
-        double currentBestScore;
-
-
-        int number = 0;
-        while(number < maxNumber && !valuesMap.keySet().isEmpty()){
-            currentBestScore = higher?0.:Double.MAX_VALUE;
-            for(Iterator<Map.Entry<UUID, Double>> entries = valuesMap.entrySet().iterator(); entries.hasNext(); ) {
-                Map.Entry<UUID, Double> entry = entries.next();
-                if(higher){
-                    if(entry.getValue() > currentBestScore){
-                        currentBestScore = entry.getValue();
-                        currentBestUuid = entry.getKey();
-                    }
-                } else {
-                    if(entry.getValue() < currentBestScore){
-                        currentBestScore = entry.getValue();
-                        currentBestUuid = entry.getKey();
-                    }
-                }
-            }
-            GameBox.debug("Found rank " + (number + 1) + " with time: " + currentBestScore + "      higher: " + higher);
-            toReturn.add(new Stat(currentBestUuid, valuesMap.get(currentBestUuid)));
-            number++;
-            valuesMap.remove(currentBestUuid);
-        }
-        return toReturn;
-    }
+    public abstract ArrayList<Stat> getTopList(String gameID, String gameTypeID, SaveType saveType, int maxNumber);
 
     public class Stat{
         private double value;
         private UUID uuid;
 
-        private Stat(UUID uuid, double value){
+        Stat(UUID uuid, double value){
             this.uuid = uuid;
             this.value = value;
         }
