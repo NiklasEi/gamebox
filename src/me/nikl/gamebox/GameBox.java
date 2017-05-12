0b24b439f083c519bbbad8440796d750f046181b
Niklas Eicker <nikle1@gmx.de>
2017-05-12 13:39:50 +0200

    moved setting to GameBoxSettings and supply chatColor to the games

--- src/me/nikl/gamebox/GameBox.java
+++ src/me/nikl/gamebox/GameBox.java
@@ -15,6 +15,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.*;
 import java.util.UUID;
@@ -28,13 +29,10 @@ import java.util.logging.Level;
 public class GameBox extends JavaPlugin{
 
 	// enable debug mode (print debug messages)
-	public static final boolean debug = true;
+	public static final boolean debug = false;
 
 	// toggle to stop inventory contents to be restored when a new gui is opened and automatically closes the old one
 	public static boolean openingNewGUI = false;
-
-	// toggle for playing sounds
-	public static boolean playSounds = false;
 
 	public static final int GAME_STARTED = 1, GAME_NOT_STARTED_ERROR = 0, GAME_NOT_ENOUGH_MONEY = 2, GAME_NOT_ENOUGH_MONEY_1 = 3, GAME_NOT_ENOUGH_MONEY_2 = 4;
 	
@@ -43,16 +41,16 @@ public class GameBox extends JavaPlugin{
 	
 	// nms util
 	private NMSUtil nms;
+
+	// API
+	private GameBoxAPI api;
 	
 	// economy
 	public static Economy econ = null;
-	private boolean econEnabled, tokensEnabled;
-	
+
 	// language file
 	public Language lang;
 
-	public static boolean useMysql = false;
-	
 	/*
 	 * Plugin manager that manages all game managers
 	 * Listens to events and passes them on
@@ -63,15 +61,12 @@ public class GameBox extends JavaPlugin{
 
 	private Statistics statistics;
 
+	@Deprecated
+	public static boolean playSounds = true;
+
 	public boolean delayedInventoryUpdate = false;
 
 
-	// time in seconds for inputs and invitations
-	public static int timeForPlayerInput;
-	public static int timeForInvitations;
-
-	
-	
 	@Override
 	public void onEnable(){
 		// get the version and set up nms
@@ -93,30 +88,30 @@ public class GameBox extends JavaPlugin{
 			return;
 		}
 
-		GameBoxSettings.loadSettings(this);
-
-		useMysql = config.getBoolean("mysql.enabled", false);
-		if(useMysql) {
-			useMysql = false;
-		}
-
-		// check again
-		// if connecting to the database failed useMysql will be set to false and the plugin should fall back to file storage
-		if(!useMysql) {
-			this.statistics = new StatisticsFile(this);
-			if (!statistics.load()) {
-				Bukkit.getLogger().log(Level.SEVERE, " Something went wrong with the data file");
-				Bukkit.getPluginManager().disablePlugin(this);
-				return;
-			}
-		}
-
 		if (!reload()) {
 			getLogger().severe(" Error while loading the plugin! Plugin was disabled!");
 
 			Bukkit.getPluginManager().disablePlugin(this);
 			return;
 		}
+
+		// check for registered games
+		new BukkitRunnable(){
+
+			@Override
+			public void run() {
+				if(PluginManager.gamesRegistered == 0){
+					Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "+ - + - + - + - + - + - + - + - + - + - + - + - + - + - +");
+					Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " There are no registered games!");
+					Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " All games are add-ons");
+					Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " You should visit Spigot and get a few ;)");
+					Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + "   https://www.spigotmc.org/resources/37273/");
+					Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "+ - + - + - + - + - + - + - + - + - + - + - + - + - + - +");
+				} else {
+					Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.GREEN + " " + PluginManager.gamesRegistered + " games were registered. Have fun :)");
+				}
+			}
+		}.runTaskLaterAsynchronously(this, 100);
 	}
 	
 	/***
@@ -126,38 +121,54 @@ public class GameBox extends JavaPlugin{
 	 * set up economy if enabled
 	 */
 	public boolean reload(){
-		timeForInvitations = config.getInt("timeForInvitations");
-		timeForPlayerInput = config.getInt("timeForPlayerInput");
-
-
 		this.lang = new Language(this);
 
-		playSounds = config.getBoolean("guiSettings.playSounds");
-		
-		// set up the economy if enabled in the configuration
-		this.econEnabled = false;
-		if(GameBox.debug)Bukkit.getConsoleSender().sendMessage("econ enabled: " + getConfig().getBoolean("economy.enabled"));
-		if(getConfig().getBoolean("economy.enabled")){
-			this.econEnabled = true;
+		this.api = new GameBoxAPI(this);
+
+		// load all settings from config
+		GameBoxSettings.loadSettings(this);
+
+		// ToDo: remove when removing all deprecated stuff
+		playSounds = GameBoxSettings.playSounds;
+
+		// disable mysql
+		GameBoxSettings.useMysql = false;
+
+		// here try connecting to database when option set in config (for later)
+
+		// if connecting to the database failed useMysql will be set to false and the plugin should fall back to file storage
+		if(!GameBoxSettings.useMysql) {
+			this.statistics = new StatisticsFile(this);
+			if (!statistics.load()) {
+				Bukkit.getLogger().log(Level.SEVERE, " Something went wrong with the data file");
+				return false;
+			}
+		}
+
+		if(GameBoxSettings.econEnabled){
 			if (!setupEconomy()){
 				Bukkit.getLogger().log(Level.SEVERE, "No economy found!");
 				return false;
 			}
 		}
-		tokensEnabled = config.getBoolean("economy.tokens.enabled", true);
 
 		// if it's not null disable first then get a new manager
 		if(pManager != null){
 			pManager.shutDown();
 			pManager = null;
 		}
+
 		// get a new plugin manager and set the other managers and handlers
 		pManager = new PluginManager(this);
 		pManager.setGuiManager(new GUIManager(this));
+
 		pManager.setHandleInviteInput(new HandleInviteInput(this));
 		pManager.setHandleInvitations(new HandleInvitations(this));
+
+		// load players that are already online (otherwise done on join)
 		pManager.loadPlayers();
-		// set cmd executor
+
+		// set cmd executors
 		mainCommand = new MainCommand(this);
 		this.getCommand("gamebox").setExecutor(mainCommand);
 		this.getCommand("gameboxadmin").setExecutor(new AdminCommand(this));
@@ -165,9 +176,11 @@ public class GameBox extends JavaPlugin{
 		return true;
 	}
 
+
 	public Statistics getStatistics(){
 		return this.statistics;
 	}
+
 
 	private boolean setupEconomy() {
 		if (getServer().getPluginManager().getPlugin("Vault") == null) {
@@ -180,7 +193,8 @@ public class GameBox extends JavaPlugin{
 		econ = rsp.getProvider();
 		return econ != null;
 	}
-	
+
+
 	private boolean setUpNMS() {
 		String version;
 		
@@ -227,41 +241,52 @@ public class GameBox extends JavaPlugin{
 		}
 		return nms != null;
 	}
-	
+
+
 	@Override
 	public void onDisable(){
 		if(pManager != null) pManager.shutDown();
 		if(statistics != null) statistics.save();
 	}
-	
+
+
 	@Override
 	public FileConfiguration getConfig() {
 		return config;
 	}
-	
+
+
 	public PluginManager getPluginManager() {
 		return pManager;
 	}
-	
+
+
+	/**
+	 * Will be removed
+	 *
+	 * as of @version 1.3.0 this should be retrieved from GameBoxSettings directly
+	 * @return
+	 */
+	@Deprecated
 	public boolean getEconEnabled() {
-		return econEnabled;
+		return GameBoxSettings.econEnabled;
 	}
-	
-	public String chatColor(String message) {
-		return ChatColor.translateAlternateColorCodes('&', message);
-	}
-	
+
+
 	public NMSUtil getNMS() {
 		return nms;
 	}
+
 
 	public static void debug(String message){
 		if(debug) Bukkit.getConsoleSender().sendMessage(message);
 	}
 
+
 	public MainCommand getMainCommand(){
 		return this.mainCommand;
 	}
+
 
 	public boolean reloadConfiguration(){
 
@@ -281,16 +306,43 @@ public class GameBox extends JavaPlugin{
 		return true;
 	}
 
+
 	public boolean wonTokens(UUID player, int tokens, String gameID){
-		if(!tokensEnabled) return false;
+		if(!GameBoxSettings.tokensEnabled) return false;
 		return this.pManager.wonTokens(player, tokens, gameID);
 	}
 
+
+	/**
+	 * Will be removed
+	 *
+	 * as of @version 1.3.0 this should be retrieved from GameBoxSettings directly
+	 * @return
+	 */
+	@Deprecated
 	public boolean isTokensEnabled() {
-		return tokensEnabled;
+		return GameBoxSettings.tokensEnabled;
 	}
 
+
+	/**
+	 * Will be removed
+	 *
+	 * as of @version 1.3.0 this should be done with GameBoxSettings directly
+	 * @return
+	 */
+	@Deprecated
 	public void setTokensEnabled(boolean tokensEnabled){
-		this.tokensEnabled = tokensEnabled;
+		GameBoxSettings.tokensEnabled = tokensEnabled;
+	}
+
+
+	public GameBoxAPI getApi(){
+		return this.api;
+	}
+
+
+	public static String chatColor(String message){
+		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 }
