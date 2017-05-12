48a6dd2e9faff84d34b6a0e5b6a3fa6ff90c1f6a
Niklas Eicker <nikle1@gmx.de>
2017-05-12 13:40:09 +0200

    manage GameBox settings

--- src/me/nikl/gamebox/GameBoxSettings.java
+++ src/me/nikl/gamebox/GameBoxSettings.java
@@ -10,11 +10,28 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class GameBoxSettings {
     public static boolean exceptInvitesWithoutPlayPermission = false;
 
+    //toggle for playing sounds
+    public static boolean playSounds = true;
+
+    public static boolean useMysql = false;
+
+    // time in seconds for inputs and invitations
+    public static int timeForPlayerInput = 30;
+    public static int timeForInvitations = 30;
+
+    public static boolean econEnabled = false;
+    public static boolean tokensEnabled = false;
 
 
     public static void loadSettings(GameBox plugin){
         FileConfiguration config = plugin.getConfig();
 
+        tokensEnabled = config.getBoolean("economy.tokens.enabled", false);
+        econEnabled = config.getBoolean("economy.enabled", false);
+        playSounds = config.getBoolean("guiSettings.playSounds", true);
+        timeForInvitations = config.getInt("timeForInvitations", 30);
+        timeForPlayerInput = config.getInt("timeForPlayerInput", 30);
+        useMysql = config.getBoolean("mysql.enabled", false);
         exceptInvitesWithoutPlayPermission = config.getBoolean("settings.exceptInvitesWithoutPlayPermission", false);
     }
 }
