1528fb8b836d386716c6490268e5fa1dbf8328ad
Niklas Eicker <nikle1@gmx.de>
2017-05-29 14:45:52 +0200

    add method to create item with given text

--- src/me/nikl/gamebox/util/ItemStackUtil.java
+++ src/me/nikl/gamebox/util/ItemStackUtil.java
@@ -2,6 +2,10 @@ package me.nikl.gamebox.util;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.List;
 
 /**
  * Created by Niklas on 13.04.2017.
@@ -43,5 +47,25 @@ public class ItemStackUtil {
             //noinspection deprecation
             return (mat == null ? null : new ItemStack(mat, 1));
         }
+    }
+
+
+    public static ItemStack createBookWithText(List<String> text) {
+        return createItemWithText(text, new MaterialData(Material.BOOK_AND_QUILL));
+    }
+
+    public static ItemStack createItemWithText(List<String> text, MaterialData materialData) {
+        ItemStack helpItem = materialData.toItemStack(1);
+
+        ItemMeta meta = helpItem.getItemMeta();
+        if(text != null) {
+            if(text.size() > 0)meta.setDisplayName(text.get(0));
+            if(text.size() > 1){
+                text.remove(0);
+                meta.setLore(text);
+            }
+        }
+        helpItem.setItemMeta(meta);
+        return helpItem;
     }
 }
