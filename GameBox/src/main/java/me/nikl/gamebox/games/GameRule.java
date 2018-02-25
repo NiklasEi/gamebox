2ba77dc661897216225729cef8be565e4566d46d
Niklas Eicker <nikle1@gmx.de>
2018-02-25 17:11:05 +0100

    add more flexibility in default game rules

--- GameBox/src/main/java/me/nikl/gamebox/games/GameRule.java
+++ GameBox/src/main/java/me/nikl/gamebox/games/GameRule.java
@@ -5,48 +5,17 @@ import me.nikl.gamebox.data.toplist.SaveType;
 /**
  * @author Niklas Eicker
  */
-public class GameRule {
+public abstract class GameRule {
     protected SaveType saveType;
     protected boolean saveStats;
     protected String key;
-
     protected double moneyToPay;
-    protected double minOrMaxScore;
-    protected double moneyToWin;
-    protected int tokenToWin;
 
-    public GameRule(String key, boolean saveStats, SaveType saveType, double moneyToPay) {
+    GameRule(String key, boolean saveStats, SaveType saveType, double moneyToPay) {
         this.saveType = saveType;
         this.saveStats = saveStats;
         this.key = key;
         this.moneyToPay = moneyToPay;
-        minOrMaxScore = saveType.isHigherScore()?0:Double.MAX_VALUE;
-        moneyToWin = 0;
-        tokenToWin = 0;
-    }
-
-    public void setToken(int token) {
-        this.tokenToWin = token;
-    }
-
-    public int getTokenToWin(){
-        return tokenToWin;
-    }
-
-    public void setMoneyToWin(double money){
-        this.moneyToWin = money;
-    }
-
-    public double getMoneyToWin(){
-        return moneyToWin;
-    }
-
-    public void setMinOrMaxScore(double minOrMaxScore){
-        this.minOrMaxScore = minOrMaxScore;
-    }
-
-    public double getMinOrMaxScore(){
-        return minOrMaxScore;
     }
 
     public SaveType getSaveType() {
