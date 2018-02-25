d7a679ff18ded4cc905dbf20558b6d98c390beca
Niklas Eicker <nikle1@gmx.de>
2018-02-25 23:32:15 +0100

    use new class with rewards

--- GameBox/src/main/java/me/nikl/gamebox/games/connectfour/CFGameRules.java
+++ GameBox/src/main/java/me/nikl/gamebox/games/connectfour/CFGameRules.java
@@ -1,23 +1,20 @@
 package me.nikl.gamebox.games.connectfour;
 
 import me.nikl.gamebox.data.toplist.SaveType;
-import me.nikl.gamebox.games.GameRule;
+import me.nikl.gamebox.games.GameRuleRewards;
 
 /**
  * @author Niklas Eicker
  *
  *         This class stores settings for a game mode
  */
-public class CFGameRules extends GameRule {
-
+public class CFGameRules extends GameRuleRewards {
     private int timePerMove, minNumberOfPlayedChips;
     private double cost;
 
     public CFGameRules(String key, int timePerMove, int minNumberOfPlayedChips, double cost, double reward, int tokens, boolean saveStats) {
-        super(key, saveStats, SaveType.WINS, cost);
+        super(key, saveStats, SaveType.WINS, cost, reward, tokens);
         this.cost = cost;
-        setMoneyToWin(reward);
-        setTokenToWin(tokens);
         this.timePerMove = timePerMove;
         this.minNumberOfPlayedChips = minNumberOfPlayedChips;
     }
