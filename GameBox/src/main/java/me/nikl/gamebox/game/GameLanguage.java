c1dbd47142796ad0c164158a64bde667ba15c925
Niklas Eicker <nikle1@gmx.de>
2018-03-05 01:43:22 +0100

    load pay messages in abstract game language

--- GameBox/src/main/java/me/nikl/gamebox/game/GameLanguage.java
+++ GameBox/src/main/java/me/nikl/gamebox/game/GameLanguage.java
@@ -10,14 +10,14 @@ import java.util.List;
  * The messages are overwritten in the specific Language files
  */
 public abstract class GameLanguage extends Language {
-
     public List<String> GAME_HELP;
-
     public String GAME_PAYED = " You payed %cost%", GAME_NOT_ENOUGH_MONEY = " Not enough money (cost: %cost%)";
 
     public GameLanguage(Game game) {
         super(game.getGameBox(), game.getModule());
 
+        this.GAME_PAYED = getString("game.econ.payed");
+        this.GAME_NOT_ENOUGH_MONEY = getString("game.econ.notEnoughMoney");
         this.GAME_HELP = getStringList("gameHelp");
     }
 }
